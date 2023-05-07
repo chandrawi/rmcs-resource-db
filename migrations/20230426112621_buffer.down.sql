eba84412bfe42f90fbcef9f8aa83473446f894c3
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2023-05-07 15:20:29 +0700

    Change buffer migration to only has 1 buffer table

--- migrations/20230426112621_buffer.down.sql
+++ migrations/20230426112621_buffer.down.sql
@@ -1,4 +1,1 @@
-DROP TABLE `buffer_timestamp_micros`;
-DROP TABLE `buffer_timestamp_index`;
-DROP TABLE `buffer_timestamp`;
-DROP TABLE `buffer_index`;
+DROP TABLE `buffer`;
--- migrations/20230426112621_buffer.up.sql
+++ migrations/20230426112621_buffer.up.sql
@@ -1,84 +1,21 @@
-CREATE TABLE `buffer_index` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `device_id` bigint(20) UNSIGNED NOT NULL,
-  `model_id` int(10) UNSIGNED NOT NULL,
-  `index` int(10) UNSIGNED NOT NULL,
-  `data` varbinary(255) NOT NULL,
-  `status` enum('DEFAULT','CONVERT','ANALYZE_GATEWAY','ANALYZE_SERVER','TRANSFER_GATEWAY','TRANSFER_SERVER','BACKUP','DELETE','ERROR') NOT NULL DEFAULT 'DEFAULT'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-CREATE TABLE `buffer_timestamp` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `device_id` bigint(20) UNSIGNED NOT NULL,
-  `model_id` int(10) UNSIGNED NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
-  `data` varbinary(255) NOT NULL,
-  `status` enum('DEFAULT','CONVERT','ANALYZE_GATEWAY','ANALYZE_SERVER','TRANSFER_GATEWAY','TRANSFER_SERVER','BACKUP','DELETE','ERROR') NOT NULL DEFAULT 'DEFAULT'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-CREATE TABLE `buffer_timestamp_index` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `device_id` bigint(20) UNSIGNED NOT NULL,
-  `model_id` int(10) UNSIGNED NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
-  `index` int(10) UNSIGNED NOT NULL,
-  `data` varbinary(255) NOT NULL,
-  `status` enum('DEFAULT','CONVERT','ANALYZE_GATEWAY','ANALYZE_SERVER','TRANSFER_GATEWAY','TRANSFER_SERVER','BACKUP','DELETE','ERROR') NOT NULL DEFAULT 'DEFAULT'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-CREATE TABLE `buffer_timestamp_micros` (
+CREATE TABLE `buffer` (
   `id` int(10) UNSIGNED NOT NULL,
   `device_id` bigint(20) UNSIGNED NOT NULL,
   `model_id` int(10) UNSIGNED NOT NULL,
   `timestamp` timestamp(6) NOT NULL DEFAULT current_timestamp(6),
+  `index` smallint(5) UNSIGNED NOT NULL,
   `data` varbinary(255) NOT NULL,
   `status` enum('DEFAULT','CONVERT','ANALYZE_GATEWAY','ANALYZE_SERVER','TRANSFER_GATEWAY','TRANSFER_SERVER','BACKUP','DELETE','ERROR') NOT NULL DEFAULT 'DEFAULT'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
-ALTER TABLE `buffer_index`
+ALTER TABLE `buffer`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `buffer_index_device_id` (`device_id`),
-  ADD KEY `buffer_index_model_id` (`model_id`);
+  ADD KEY `buffer_device_id` (`device_id`),
+  ADD KEY `buffer_model_id` (`model_id`);
 
-ALTER TABLE `buffer_timestamp`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `buffer_timestamp_device_id` (`device_id`),
-  ADD KEY `buffer_timestamp_model_id` (`model_id`);
-
-ALTER TABLE `buffer_timestamp_index`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `buffer_timestamp_index_device_id` (`device_id`),
-  ADD KEY `buffer_timestamp_index_model_id` (`model_id`);
-
-ALTER TABLE `buffer_timestamp_micros`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `buffer_timestamp_micros_device_id` (`device_id`),
-  ADD KEY `buffer_timestamp_micros_model_id` (`model_id`);
-
-ALTER TABLE `buffer_index`
+ALTER TABLE `buffer`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE `buffer_timestamp`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `buffer_timestamp_index`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `buffer_timestamp_micros`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `buffer_index`
-  ADD CONSTRAINT `buffer_index_device_id` FOREIGN KEY (`device_id`) REFERENCES `device` (`device_id`),
-  ADD CONSTRAINT `buffer_index_model_id` FOREIGN KEY (`model_id`) REFERENCES `model` (`model_id`);
-
-ALTER TABLE `buffer_timestamp`
-  ADD CONSTRAINT `buffer_timestamp_device_id` FOREIGN KEY (`device_id`) REFERENCES `device` (`device_id`),
-  ADD CONSTRAINT `buffer_timestamp_model_id` FOREIGN KEY (`model_id`) REFERENCES `model` (`model_id`);
-
-ALTER TABLE `buffer_timestamp_index`
-  ADD CONSTRAINT `buffer_timestamp_index_device_id` FOREIGN KEY (`device_id`) REFERENCES `device` (`device_id`),
-  ADD CONSTRAINT `buffer_timestamp_index_model_id` FOREIGN KEY (`model_id`) REFERENCES `model` (`model_id`);
-
-ALTER TABLE `buffer_timestamp_micros`
-  ADD CONSTRAINT `buffer_timestamp_micros_device_id` FOREIGN KEY (`device_id`) REFERENCES `device` (`device_id`),
-  ADD CONSTRAINT `buffer_timestamp_micros_model_id` FOREIGN KEY (`model_id`) REFERENCES `model` (`model_id`);
+ALTER TABLE `buffer`
+  ADD CONSTRAINT `buffer_device_id` FOREIGN KEY (`device_id`) REFERENCES `device` (`device_id`),
+  ADD CONSTRAINT `buffer_model_id` FOREIGN KEY (`model_id`) REFERENCES `model` (`model_id`);
