1971651a37f52d1331bbed6bb7143285905c1712
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2024-07-25 20:03:29 +0700

    Create database migration for set

---
+++ migrations/20240725015942_set.down.sql
@@ -0,0 +1,4 @@
+DROP TABLE "set_map";
+DROP TABLE "set";
+DROP TABLE "set_template_map";
+DROP TABLE "set_template";
---
+++ migrations/20240725015942_set.up.sql
@@ -0,0 +1,47 @@
+CREATE TABLE IF NOT EXISTS "set_template" (
+  "template_id" uuid NOT NULL,
+  "name" varchar(64) NOT NULL,
+  "description" varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY ("template_id")
+);
+
+CREATE TABLE IF NOT EXISTS "set_template_map" (
+  "template_id" uuid NOT NULL,
+  "type_id" uuid NOT NULL,
+  "model_id" uuid NOT NULL,
+  "data_index" bytea,
+  "template_index" smallint NOT NULL DEFAULT 0,
+  PRIMARY KEY ("template_id","template_index"),
+  FOREIGN KEY ("template_id")
+    REFERENCES "set_template" ("template_id") ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY ("type_id")
+    REFERENCES "device_type" ("type_id") ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY ("model_id")
+    REFERENCES "model" ("model_id") ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS "set" (
+  "set_id" uuid NOT NULL,
+  "template_id" uuid NOT NULL,
+  "name" varchar(64) NOT NULL,
+  "description" varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY ("set_id"),
+  FOREIGN KEY ("template_id")
+    REFERENCES "set_template" ("template_id") ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS "set_map" (
+  "set_id" uuid NOT NULL,
+  "device_id" uuid NOT NULL,
+  "model_id" uuid NOT NULL,
+  "data_index" bytea,
+  "set_position" smallint NOT NULL DEFAULT 0,
+  "set_number" smallint NOT NULL DEFAULT 0,
+  PRIMARY KEY ("set_id","device_id","model_id"),
+  FOREIGN KEY ("set_id")
+    REFERENCES "set" ("set_id") ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY ("device_id")
+    REFERENCES "device" ("device_id") ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY ("model_id")
+    REFERENCES "model" ("model_id") ON UPDATE CASCADE ON DELETE CASCADE
+);
